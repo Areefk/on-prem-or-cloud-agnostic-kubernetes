9540840a6917ae58b39f15fe32bc03eda04acd97
Areefk <51814502+Areefk@users.noreply.github.com>
2020-12-24 11:54:13 +0530

    Update install-kubernetes.sh

--- scripts/install-kubernetes.sh
+++ scripts/install-kubernetes.sh
@@ -12,7 +12,11 @@ add-apt-repository \
    "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+#apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+sudo apt-get update && sudo apt-get install -y \
+  containerd.io=1.2.13-2 \
+  docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
+  docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
 
 echo "installing kubernetes"
 apt-get update && apt-get install -y apt-transport-https
